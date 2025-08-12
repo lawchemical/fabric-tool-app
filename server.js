75ca313104415efe6dbb637b5d1fa00d520cb61a
lawchemical <sean@lawchemical.com>
2025-08-11 20:20:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Add CORS middleware at the very top, before routes
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://americanmillsoutdoor.com' // or '*' for testing
+}));
+
 // Import your fabric routes
 const fabricRoutes = require('./fabricRoutes');
 app.use('/api', fabricRoutes);
@@ -18,7 +24,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://americanmillsoutdoor.com' // or '*' for testing
-}));
